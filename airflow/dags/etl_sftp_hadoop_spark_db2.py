7eefbe86523d3fb570a26164094404000e991ca8
henrybautista20 <henryfb2003@gmail.com>
2025-07-08 05:00:08 +0000

    update2

---
+++ airflow/dags/etl_sftp_hadoop_spark_db2.py
@@ -0,0 +1,52 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.providers.ssh.operators.ssh import SSHOperator
+
+from datetime import datetime
+jars = [
+    "/opt/airflow/spark_jobs/jars/spark-excel_2.12-0.13.7.jar",
+    "/opt/airflow/spark_jobs/jars/poi-4.1.2.jar",
+    "/opt/airflow/spark_jobs/jars/poi-ooxml-4.1.2.jar",
+    "/opt/airflow/spark_jobs/jars/xmlbeans-3.1.0.jar",
+    "/opt/airflow/spark_jobs/jars/commons-math3-3.6.1.jar",
+    "/opt/airflow/spark_jobs/jars/ooxml-schemas-1.4.jar",
+    "/opt/airflow/spark_jobs/jars/postgresql-42.7.3.jar"
+]
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2025, 6, 24),
+    'retries': 1,
+}
+
+with DAG(
+    dag_id='etl_sftp_hadoop_spark_db2',
+    default_args=default_args,
+    schedule_interval=None,  # Manual o lo puedes poner diario, '0 6 * * *'
+    catchup=False,
+    description='Copia usuarios.csv desde /sftp-data en el contenedor hadoop-namenode a HDFS /data',
+) as dag:
+
+    upload_to_hdfs_via_ssh = SSHOperator(
+        task_id='upload_csv_hdfs_ssh',
+        ssh_conn_id='hadoop_ssh',
+        command="""
+            docker exec snp-dwh-hadoop-namenode-1 hdfs dfs -mkdir -p /data &&
+            docker exec snp-dwh-hadoop-namenode-1 hdfs dfs -put -f /sftp-data/usuarios.csv /data/
+        """
+    )
+
+    run_spark = SparkSubmitOperator(
+        task_id='run_remote_spark_job',
+          application='/opt/airflow/spark_jobs/mi_script2.py',
+    conn_id='spark_remote',
+    conf={
+            "spark.driver.memory": "4g",
+            "spark.executor.memory": "4g",
+            "spark.executor.cores": "2",
+            "spark.default.parallelism": "100",
+        },
+        verbose=True,
+   jars=",".join(jars)
+    )
+    upload_to_hdfs_via_ssh >> run_spark
